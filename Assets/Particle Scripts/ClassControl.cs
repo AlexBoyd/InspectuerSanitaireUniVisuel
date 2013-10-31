7916cd62b24166927e7385b1232eca49222a6cd4
Alex <alexander,ray.boyd@gmail.com>
2013-10-30 18:42:06 -0700

    updating flow particle sim

--- Assets/Particle Scripts/ClassControl.cs
+++ Assets/Particle Scripts/ClassControl.cs
@@ -11,7 +11,6 @@ public class ClassControl : MonoBehaviour
 	public List<ClassHookup> ClassDependancies; 
 	public ClassGenerator ClassGen;
 	public GameObject FlowParticlePrefab;
-//	private ParticleSystem.Particle[] particles;
 	
 	[Serializable]
 	public class ClassHookup
@@ -36,48 +35,75 @@ public class ClassControl : MonoBehaviour
 			ch.FlowParticles.transform.parent = transform;
 			ch.FlowParticles.transform.localPosition = Vector3.zero;
 			ch.FlowParticles.particleSystem.startColor = new Color(SewageLevel * 0.05f, 1f - (SewageLevel * 0.05f), 0f);;
-			ch.FlowParticles.particleSystem.startSize = SewageLevel / 5f;
+			ch.FlowParticles.particleSystem.startSize = SewageLevel / 2.5f;
+			ch.FlowParticles.particleSystem.emissionRate = ch.DepedancyValue * 5f;
 		}
-		
-		
+			
 	}
 	
 	private void Update()
 	{
 		InnerClassEmitter.emissionRate = SewageLevel / 2f;
-		InnerClassEmitter.startColor = new Color(SewageLevel * 0.05f, 1f - (SewageLevel * 0.05f), 0f);	
+		InnerClassEmitter.startColor = new Color(SewageLevel * 0.05f, 1f - (SewageLevel * 0.05f), 100f);	
 		
 		foreach(ClassHookup cc in ClassDependancies)
 		{
 			rigidbody.AddForce((cc.AttachedClass.gameObject.transform.position - gameObject.transform.position).normalized 
 				* Mathf.Pow((cc.AttachedClass.gameObject.transform.position - gameObject.transform.position).sqrMagnitude, 1.2f)
-				* Mathf.Pow(cc.DepedancyValue, 4f) / (500f * (Time.realtimeSinceStartup + 10)));
+				* Mathf.Pow(cc.DepedancyValue, 4f) / (800f * (Time.realtimeSinceStartup + 10)));
 		}
 		
-		rigidbody.AddForce((Vector3.zero - new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0f)).normalized * (Vector3.zero - gameObject.transform.position).sqrMagnitude * (Vector3.zero - gameObject.transform.position).sqrMagnitude / 100000000f);
+		rigidbody.AddForce((Vector3.zero - new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0f)).normalized * ManhatanDist(Vector3.zero, gameObject.transform.position).sqrMagnitude/ 80000f );
 		
 		foreach(ClassControl cc in ClassGen.Classes)
 		{
 			if(cc != this)
 			{
 				Vector3 distance = gameObject.transform.position - cc.gameObject.transform.position;
-				rigidbody.AddForce(distance.normalized * 20f / (Mathf.Pow(distance.magnitude, 1.4f)));
+				rigidbody.AddForce(distance.normalized * 100f / (Mathf.Pow(distance.magnitude, 1.4f)));
 			}	
 		}
 		//Push to Z = 0;
-		rigidbody.AddForce(new Vector3(0,0, -transform.position.z) * Mathf.Pow(Time.realtimeSinceStartup, 3) / 500f);
+		rigidbody.AddForce(new Vector3(0,0, -transform.position.z) * Mathf.Pow(Time.realtimeSinceStartup, 2) / 500f);
 		
 		
 		rigidbody.drag = Time.realtimeSinceStartup * 1f;
 	}
 	
 	void LateUpdate() 
-	{  
-		
+	{          
 		foreach(ClassHookup ch in ClassDependancies)
 		{
-        	ch.FlowParticles.transform.LookAt(ch.AttachedClass.transform);	
+			ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ch.FlowParticles.particleSystem.particleCount];
+		    int length = ch.FlowParticles.particleSystem.GetParticles(particles); 
+        	int i = 0;
+        	//ch.FlowParticles.transform.LookAt(ch.AttachedClass.transform);	
+		
+			while (i < length) 
+			{
+            	//Target is a Transform object
+
+	            Vector3 direction = ch.AttachedClass.transform.position - particles[i].position;
+	
+	            direction.Normalize();
+	     
+	            float variableSpeed = ch.FlowParticles.particleSystem.startSpeed / (particles[i].lifetime + 0.1f) + particles[i].startLifetime;
+	            particles[i].position += direction * variableSpeed * Time.deltaTime;
+	            if(Vector3.Distance(ch.AttachedClass.transform.position, particles[i].position) < 1.0f) 
+				{
+	                particles[i].lifetime = -0.1f; //Kill the particle
+	            }
+	
+	            i++;
+	        }
+	
+	        ch.FlowParticles.particleSystem.SetParticles(particles, length);
 		}
     }
+		
+	private Vector3 ManhatanDist(Vector3 v1, Vector3 v2)
+	{
+		return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+	}
 }
 	
